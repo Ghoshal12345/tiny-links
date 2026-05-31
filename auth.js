b12cba0ae3b52c33405c0cb0e4d38a3e51994fb6
Ghoshal <148778276+Ghoshal12345@users.noreply.github.com>
2026-05-31 15:43:14 +0530

    jwt expiration time

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ function handleSetUser(user) {
         _id: user._id,
         email: user.email,
         name: user.name,
-    }, secretKey, );
+    }, secretKey, {expiresIn: '15m'});
 }
 
 function handleGetUser(token) {
